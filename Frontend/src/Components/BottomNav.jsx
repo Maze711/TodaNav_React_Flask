4d4c9ad9de16d21da03a1d73116ed10a494b209a
Maze711 <badlonmazeclarion@gmail.com>
2025-05-07 12:38:56 +0800

    Enhance BottomNav component with icons and responsive styles

--- Frontend/src/Components/BottomNav.jsx
+++ Frontend/src/Components/BottomNav.jsx
@@ -4,38 +4,49 @@ import {
   MDBNavbar,
   MDBContainer,
   MDBNavbarNav,
-  MDBIcon,
 } from 'mdb-react-ui-kit';
+
+// Import local PNG icons
+import homeIcon from '../assets/ico/home.png';
+import walletIcon from '../assets/ico/wallet.png';
+import locateIcon from '../assets/ico/location.png';
+import chattingIcon from '../assets/ico/chatting.png';
+import notificationIcon from '../assets/ico/notification.png';
 
 export const BottomNav = () => {
   return (
     <>
-      <MDBNavbar fixed='bottom' light bgColor='light'>
+      <MDBNavbar fixed='bottom' light bgColor='light' className='bottom-nav'>
         <MDBContainer fluid>
-          <MDBNavbarNav className='d-flex flex-row justify-content-around w-100'>
-            <li className='nav-item'>
+          <MDBNavbarNav className='d-flex flex-row justify-content-around w-100 align-items-center'>
+            <li className='nav-item text-center'>
               <Link to='/' className='nav-link'>
-                <MDBIcon fas icon='home' /> Home
+                <img src={homeIcon} alt='Home' className='nav-icon' />
+                <div className='nav-text'>Home</div>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item text-center'>
               <Link to='/payments' className='nav-link'>
-                <MDBIcon fas icon='wallet' /> Payments
+                <img src={walletIcon} alt='Payments' className='nav-icon' />
+                <div className='nav-text'>Payments</div>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item text-center'>
               <Link to='/locate' className='nav-link'>
-                <MDBIcon fas icon='map-marker-alt' /> Locate
+                <img src={locateIcon} alt='Locate' className='nav-icon' />
+                <div className='nav-text'>Locate</div>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item text-center'>
               <Link to='/messages' className='nav-link'>
-                <MDBIcon fas icon='envelope' /> Messages
+                <img src={chattingIcon} alt='Messages' className='nav-icon' />
+                <div className='nav-text'>Messages</div>
               </Link>
             </li>
-            <li className='nav-item'>
+            <li className='nav-item text-center'>
               <Link to='/Notif' className='nav-link'>
-                <MDBIcon fas icon='bell' /> Notification
+                <img src={notificationIcon} alt='Notification' className='nav-icon' />
+                <div className='nav-text'>Notification</div>
               </Link>
             </li>
           </MDBNavbarNav>
